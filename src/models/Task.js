9d6df7eb27b444a2f47739e371a486aaa0bd45e3
Humberto <betohg0416@gmail.com>
2022-12-11 16:02:29 -0600

    first commit

---
+++ src/models/Task.js
@@ -0,0 +1,34 @@
+import { Schema, model } from "mongoose";
+
+const taskSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true
+    },
+    description: {
+      type: String,
+      required: true
+    },
+    fechaContratacion: {
+      type: Date,
+      default: Date.now
+    },
+    img: {
+      data: Buffer,
+      contentType: String
+    },
+    done: {
+      type: Boolean,
+      default: false
+    },
+  },
+  {
+    timestamps: true,
+    versionKey: false
+  }
+);
+
+export default model("Task", taskSchema);
