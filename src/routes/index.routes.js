de5670e02f2f557702b14b26b7172f01f148a361
Humberto <betohg0416@gmail.com>
2022-12-14 19:12:43 -0600

    Cambio Routes

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -1,22 +1,12 @@
 import { Router } from "express";
-import { renderTasks, createTask, renderTaskEdit, editTask, deleteTask, toggleDone } from "../controllers/tasks.controller";
+import { renderTasks } from "../controllers/tasks.controller";
 
 const router = Router();
 
-router.get("/", renderTasks);
-
-router.post('/tasks/add', createTask);
+router.get("/", renderTasks)
 
 router.get("/about", (req, res) => {
   res.render("about.hbs");
-});
-
-router.get("/edit/:id", renderTaskEdit);
-
-router.post('/edit/:id', editTask);
-
-router.get('/delete/:id', deleteTask);
-
-router.get('/done/:id', toggleDone);
+})
 
 export default router;
